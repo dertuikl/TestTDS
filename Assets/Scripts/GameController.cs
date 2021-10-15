f4acbb257b6b2c2ce53980bbb8ed1c00c5f8639e
Aleksandra Bosyuk <alexandra.bosyuk@onthespotdev.com>
2021-10-15 22:21:26 +0300

    Initial grid implementation

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2Int gridSize = new Vector2Int(7, 7);
+    
+    [SerializeField]
+    private GridLayoutGroup layoutGroup;
+
+    [SerializeField]
+    private GameObject cellPrefab;
+
+    private RectTransform gridTransform => layoutGroup.transform as RectTransform;
+    
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        layoutGroup.constraintCount = gridSize.x;
+        layoutGroup.cellSize = CalculateCellSize();
+        
+        for (int i = 0; i < gridSize.x * gridSize.y; i++) {
+            Instantiate(cellPrefab, gridTransform);
+        }
+    }
+
+    private Vector2 CalculateCellSize()
+    {
+        float length = gridTransform.rect.width / gridSize.x;
+        return Vector2.one * length;
+    }
+}
